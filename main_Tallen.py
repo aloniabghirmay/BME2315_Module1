d5dad5df6e681c93de15e20feaea4f248606982e
TallenAvellaneda <txe2nf@virginia.edu>
2026-09-14 21:30:51 -0400

    Updated main_Tallen.py
    Tried some stuff to begin being able to generate a scatterplot by searching through the column "cognitive status" to determine if a patient has dementia or not.

--- main_Tallen.py
+++ main_Tallen.py
@@ -2,5 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("C:/Users/tall1/Documents/GitHub/BME2315_Module1/Metadata and Protein Data for Module 1.csv")
 
-for header in df.columns:
-    print(header)
+i = 0
+rows = len(df)
+
+for row in df:
+    cognitive_status = row['Cognitive Status']
+    if cognitive_status == "Dementia":
+        print("True")
+    else:
+        print("False")
