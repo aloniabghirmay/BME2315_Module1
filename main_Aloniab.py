09294fc533e76082f1e4a84a18fb199ba56eb003
aloniabghirmay <gsv2jv@virginia.edu>
2026-09-17 01:48:31 -0400

    rename

--- main_Aloniab.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"C:\Users\aloni\OneDrive\Aloniab - University of Virginia\Undergraduate\BME2315 Fall 2026\Module1\BME2315_Module1\BME2315_Module1\BME2315_Module1\Metadata and Protein Data for Module 1.csv")
-
-for header in df.columns:
-    print(header)
